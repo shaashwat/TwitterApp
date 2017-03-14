0ba88b807e6d754fe3832f81432cac43d514b549
shaashwat <shaash317@gmail.com>
2017-03-14 15:54:04 -0400

    added tweet downloading script

--- getTweets.py
+++ getTweets.py
@@ -1,5 +1,15 @@
 import twitter
 
-api = twitter.Api()
-statuses = api.GetUserTimeline(26257166)
+api = twitter.Api(consumer_key="iFM83lSHaysa7GTHbGRYOUHbH",
+                  consumer_secret = "FgcO8CrxEYedFWUjn43aChntaspJV5FPbQamCI4Dp6R5ovQoT0",
+                  access_token_key="722173364396298240-UnOQ1uZiwAETRNy5Jh4V8vWGh2JazPC",
+                  access_token_secret="xgPt5BwXLziBBzMZb4fsYl8d7lZRP4iL5ainlvMhX3mug"
+                  )
+
+#status = api.PostUpdate('I love python-twitter!')
+#print(status.text)
+
+statuses = api.GetUserTimeline(user_id=26257166, count=100)
 print([s.text for s in statuses])
+
+#user = api.GetUser(user_id=26257166)
