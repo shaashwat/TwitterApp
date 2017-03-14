398c3c830a532e0de00fda4f08325258430ced88
shaashwat <shaash317@gmail.com>
2017-03-14 16:15:50 -0400

    first working version created

--- MarkovModel.py
+++ MarkovModel.py
@@ -1,16 +1,13 @@
 import markovify
 
 # Get raw text as string.
-with open("/home/shaash/Documents/PythonProjects/TwitterBot/StatesNotebookFinal.txt") as f:
-    text = f.read()
 
-# Build the model.
-text_model = markovify.Text(text)
 
-# Print five randomly-generated sentences
-for i in range(5):
-    print(text_model.make_sentence())
+def create_markov_model():
+    with open("DownloadedTweets.txt") as f:
+        text = f.read()
 
-# Print three randomly-generated sentences of no more than 140 characters
-for i in range(3):
-    print(text_model.make_short_sentence(140))
+    # Build the model.
+    text_model = markovify.Text(text)
+
+    return text_model.make_short_sentence(140)
