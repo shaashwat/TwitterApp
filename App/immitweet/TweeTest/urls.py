5400379a4888fa73d308fcb0c96ed682a07441ef
dakeenekid <dbkeene.tsyc@gmail.com>
2017-03-18 21:36:16 -0400

    App sorta works

--- App/immitweet/TweeTest/urls.py
+++ App/immitweet/TweeTest/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from TweeTest import views
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
+    url(r'^$', views.HomePageView.as_view(), name='index'),
+    url(r'^result$', views.ResultPageView.as_view(), name='result')
 ]
+
