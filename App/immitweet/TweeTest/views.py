dcd9781fe9c7ede9114bc4c972d423ac212244d6
dakeenekid <dbkeene.tsyc@gmail.com>
2017-03-19 21:40:24 -0400

    App works with updated script

---
+++ App/immitweet/TweeTest/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+from utils import runThis
+
+# Create your views here.
+
+# Current view to display the index html site. If it recognizes a POST request, it will display the result as plain text.
+def view_name(request):
+    if request.method == "POST":
+        screenname = request.POST.get("handle", None)
+        result = runThis.getTweets(screenname)
+        return HttpResponse(result, content_type="text/plain")
+    return render(request, 'index.html', {})
+
+
+# Create your views here.
