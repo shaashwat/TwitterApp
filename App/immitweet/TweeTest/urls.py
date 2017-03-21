b3641f0750ee38c2a95ed4cde62fe00ffdf0070e
dakeenekid <dbkeene.tsyc@gmail.com>
2017-03-20 22:15:31 -0400

    Website v.0.1.0

--- App/immitweet/TweeTest/urls.py
+++ App/immitweet/TweeTest/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from TweeTest import views
 
 urlpatterns = [
-    url(r'^$', views.view_name, name='home'),
+    url(r'^$', views.view_home, name='home'),
     url(r'^about/$', views.AboutPageView.as_view()),
     url(r'^contact/$', views.ContactPageView.as_view()),
+    url(r'^result/$', views.view_tweet, name='result'),
+
 
 ]
 
