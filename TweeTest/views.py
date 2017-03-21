0d6f4caa5c7edfe7b7d94e4049d0eb94e998f880
dakeenekid <dbkeene.tsyc@gmail.com>
2017-03-20 21:53:48 -0400

    Updated HTML

--- TweeTest/views.py
+++ TweeTest/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.generic import TemplateView
 
 from utils import runThis
 
 # Create your views here.
 
 # Current view to display the index html site. If it recognizes a POST request, it will display the result as plain text.
-def view_name(request):
+def view_home(request):
     if request.method == "POST":
         screenname = request.POST.get("handle", None)
         result = runThis.getTweets(screenname)
         return HttpResponse(result, content_type="text/plain")
     return render(request, 'index.html', {})
+
+def view_tweet(request):
+    return render(request,'result.html')
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+class ContactPageView(TemplateView):
+    template_name = "contact.html"
+
 
 
 # Create your views here.
