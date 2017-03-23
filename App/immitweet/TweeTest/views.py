4675cd7184ef11d518421139f7286cc25d5dec07
dakeenekid <dbkeene.tsyc@gmail.com>
2017-03-22 20:59:50 -0400

    App returns new tweet to seperate page.

--- App/immitweet/TweeTest/views.py
+++ App/immitweet/TweeTest/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.template import Context, loader
 
 from utils import runThis
 
@@ -11,7 +12,8 @@ def view_home(request):
     if request.method == "POST":
         screenname = request.POST.get("handle", None)
         result = runThis.getTweets(screenname)
-        return render(request, 'result.html,', {'result',result})
+        page = loader.get_template("result.html")
+        return render(request,'result.html',{"result":result})
     return render(request, 'index.html', {})
 
 class AboutPageView(TemplateView):
