c4c14caa8c8a462d8e07243f14a0744d373abcb1
dakeenekid <dbkeene.tsyc@gmail.com>
2017-03-22 22:04:04 -0400

    Web App v.0.2.0

--- TweeTest/views.py
+++ TweeTest/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.template import Context, loader
 
 from utils import runThis
 
@@ -11,11 +12,11 @@ def view_home(request):
     if request.method == "POST":
         screenname = request.POST.get("handle", None)
         result = runThis.getTweets(screenname)
-        return HttpResponse(result, content_type="text/plain")
-    return render(request, 'index.html', {})
+        page = loader.get_template("result.html")
 
-def view_tweet(request):
-    return render(request,'result.html')
+        return render(request,'result.html',{"result":result, "screenname":screenname})
+
+    return render(request, 'index.html', {})
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
@@ -23,6 +24,8 @@ class AboutPageView(TemplateView):
 class ContactPageView(TemplateView):
     template_name = "contact.html"
 
+class ResultPageView(TemplateView):
+    template_name = "result.html"
 
 
 # Create your views here.
